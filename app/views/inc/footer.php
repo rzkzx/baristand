ccd5de45ebb168e7aa29752c0714812d0291608d
rzkzx <romanzidanramadhan@gmail.com>
2022-05-18 09:59:07 +0800

    Change Title

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -6,7 +6,7 @@
     <div class="modal-dialog" role="document">
     <div class="modal-content">
         <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabelLogout">BARISTAND</h5>
+        <h5 class="modal-title" id="exampleModalLabelLogout">BSPJI BANJARBARU</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
